072a892cdf22c1ad212eae9ee83fcd907d6a24c6
colin27091 <colinregourd@gmail.com>
2018-09-26 16:46:57 +0200

    startNewGame

--- src/main/java/bowling2/MultiPlayerGame.java
+++ src/main/java/bowling2/MultiPlayerGame.java
@@ -5,24 +5,48 @@
  */
 package bowling2;
 
+import bowling.SinglePlayerGame;
+
 /**
  *
  * @author c
  */
 public class MultiPlayerGame implements bowling.MultiPlayerGame{
+    
+    String[] playerName;
+    SinglePlayerGame[] player;
+    int currentPlayer;
 
     @Override
     public String startNewGame(String[] playerName) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        currentPlayer = 0;
+        if (playerName.length == 0 ){
+            throw new IllegalArgumentException("Il doit y avoir au moins un joueur"); 
+        }
+        for( int i = 0; i < playerName.length; i++){
+            player[i] =  new SinglePlayerGame();
+        }
+        playerName = playerName;
+        return currentPlayerFrame();
     }
 
     @Override
     public String lancer(int nombreDeQuillesAbattues) throws Exception {
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public int scoreFor(String playerName) throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public String currentPlayerFrame(){
+        /*Prochain tir : joueur Bastide, tour n° 5, boule n° 1*/
+        String str = "Prochain tir : ";
+        str = str + "joueur " + playerName[currentPlayer];/*joueur Bastide*/
+        str = str + ", tour n° " + player[currentPlayer].getCurrentFrame().getFrameNumber(); /*tour n° 5*/
+        str = str + ", boule n° " + player[currentPlayer].getCurrentFrame().getBallsThrown();
+        return str;
     }
 }
