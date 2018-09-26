3f13b2efcf68248e450d356092dcb3c482ce64ce
colin27091 <colinregourd@gmail.com>
2018-09-26 16:16:59 +0200

    initProjet

---
+++ src/main/java/bowling2/MultiPlayerGame.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bowling2;
+
+/**
+ *
+ * @author c
+ */
+public class MultiPlayerGame implements bowling.MultiPlayerGame{
+
+    @Override
+    public String startNewGame(String[] playerName) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String lancer(int nombreDeQuillesAbattues) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int scoreFor(String playerName) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
